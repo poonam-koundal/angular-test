f59984532cf9a62c15c7368647e54359b3e36c55
poonamk@smartdatainc.net <poonamk@smartdatainc.net>
2019-02-28 13:37:42 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //ref to this component in
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
